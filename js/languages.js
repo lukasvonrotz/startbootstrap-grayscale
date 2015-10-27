b8a3e616931625e54d624acf30592cba79f144e5
unknown <lukas.v.rotz@hotmail.com>
2015-10-27 22:55:20 +0100

    content

---
+++ js/languages.js
@@ -0,0 +1,51 @@
+function autoResize(id){
+	var newwidth = document.getElementById(id).offsetWidth;
+	var newheight = (newwidth/1.68) + 'px';
+	document.getElementById(id).height = newheight;
+}
+function englishRemember(){	
+	document.getElementById("video-remember1").innerHTML = "In loving memories of 'Länzi'";		
+	document.getElementById("video-remember2").innerHTML = "&#8224; 01. January 1960 - 01. January 2000";
+	document.getElementById("video-remember3").innerHTML = "Remember the times we could enjoy, your humor and all your lovely deeds. Remember the fun we had that day, when we built castles at the beach. Remember the gleaming in your face, when you were in your beloved nature. Remember your favourable way, which let us look up to you.</br></br><a class ='btn btn-default btn-sm' href='www.noisetrade.com/trainwood/death-is-in-this-land-single'>Order CD (Single)</a>";
+
+}
+function germanRemember(){
+	document.getElementById("video-remember1").innerHTML = "In Gedenken an 'Länzi'";
+	document.getElementById("video-remember2").innerHTML = "&#8224; 01. Januar 1960 - 01. Januar 2000";
+	document.getElementById("video-remember3").innerHTML = "Wir erinnern uns an deine gutmütige Art, an deinen Humor, an die vielen schönen Momente welche wir mit dir verbringen durften.</br>We wanna thank you for everything.<br></br><a class ='btn btn-default btn-sm' href='www.noisetrade.com/trainwood/death-is-in-this-land-single'>Bestellung CD (Single)</a></br>";
+}
+function englishDeath(){	
+	document.getElementById("video-death").innerHTML = "Through the 'Death Is In This Land' videoclip we would like to highlight that the current situation in Iraq and Syria is not only happening on TV screens and in newspapers. To the people there, this is their daily, brutal reality. Through our music, we try, as far as possible, to enter into this reality - to really hear and see what is happening in these far away lands. We sing these lyrics to, at least in some small way, be with these people in the Middle East in thought and compassion. The song can be downloaded free of charge <a href='www.noisetrade.com/trainwood/death-is-in-this-land-single'>here</a>.";
+}
+function germanDeath(){
+	document.getElementById("video-death").innerHTML = "Mit dem Videoclip „Death Is In This Land“ wollen wir ins Gedächtnis rufen, dass die aktuellen Geschehnisse in Irak und Syrien sich nicht nur auf den TV-Bildschirmen und in Zeitungen abspielen. Für andere Menschen ist das die tägliche, brutale Realität. Mit Hilfe unserer Musik versuchen wir, so gut es geht, wirklich und von innen her zu hören und zu sehen, was da weit weg von uns passiert. Wir singen diese Zeilen, um mit unseren Gedanken und unserem Mitgefühl ein wenig bei den Menschen im Nahen Osten zu sein. Der Song zum Videoclip kann <a href='www.noisetrade.com/trainwood/death-is-in-this-land-single'>hier</a> gratis heruntergeladen werden.";
+}
+function englishLive(){	
+	document.getElementById("live1").innerHTML = "No shows planned at the moment. </br>Contact us on <a href='mailto:band@trainwood.com'>band@trainwood.com</a> to send a request.";
+}
+function germanLive(){
+	document.getElementById("live1").innerHTML = "Momentan sind keine Auftritte geplant. </br>Schreib uns auf <a href='mailto:band@trainwood.com'>band@trainwood.com</a> um eine Anfrage zu schicken.";
+}
+function englishBand(){	
+	document.getElementById("band1").innerHTML = "The beginning of 2012. Michael & Lukas von Rotz get plenty of miles under their belts to rack their minds for music with Alfred Perner. Fine-tuning their sound until deep into the morning hours until finally the first songs are ready. Happy with the results, some good stuff produced, all the effort seems to have been worth it. Dominik Omlin enters the scene and now Trainwood can really get moving. Their shared passion for music brings them closer together and initial concerts take place in Switzerland and Germany.";
+	document.getElementById("band2").innerHTML = "In 2013 the band is severely challenged by illness, a sudden bereavement of a close relative and inner turmoil. The search for a fifth band member to compliment and complete their sound is not easy. In this state of apparent weakness, Trainwood paradoxically finds the strength which becomes their lifestyle: 'Trust and never give up!'";
+	document.getElementById("band3").innerHTML = "Autumn 2014. Lorenz Imfeld joins the band and soon thereafter the recording studio and film camera beckon. The resulting videoclip: “Death is in this land“ – based on the current situation in Iraq and Syria. With over 2500 clicks on YouTube by now, Trainwood is very proud to have got through to, and hopefully also affected, that many people.";
+	document.getElementById("band4").innerHTML = "The beginning of 2015, Alfred Perner announces his resignation! Thank you Alfred for your support and your raised energy. Without you, we wouldn't be where we are now. Luckily, Adrian Gander replaced Alfred on the bass guitar shortly afterwards. With the goal of a second studio record, the Song 'Remember', Trainwood was back in the band room.";
+	document.getElementById("band5").innerHTML = "Trainwood are grateful to be able to move on together despite the difficulties and challenges – whether close to home or in distant lands. Music is not just about putting rhythm and sound together – it is a safe haven, an expression of the greatest joy and hope and a language everybody understands. And that makes everything worthwhile…";
+
+}
+function germanBand(){
+	document.getElementById("band1").innerHTML = "Anfangs 2012. Michael & Lukas von Rotz legen einige Kilometer zurück, um sich mit Alfred Perner den Kopf über Musik zu zerbrechen. Sie feilen teilweise bis tief in die Nacht an ihrem Sound, bis die ersten Songs da sind. Zufrieden, dass das Zeug schon mal ganz gut klingt und sich die Sache also gelohnt zu haben scheint, bekommen sie Verstärkung von Dominik Omlin. Nun kommt Trainwood ins Rollen. Die Begeisterung der Vier für ihre Musik bringt sie näher zusammen und es folgen erste Konzerte in der Schweiz und Deutschland.";
+	document.getElementById("band2").innerHTML = "2013 fordern Krankheit, ein plötzlicher Todesfall im engeren Verwandtenkreis und auch innere Konflikte die Band jedoch enorm heraus. Zudem fehlt es dem Sound immer noch an einem weiteren Bandmitglied, das sich einfach nicht finden lassen will. Aber gerade in dieser Schwäche erfährt Trainwood, so paradox es klingt, Stärke, was fortan ihren Lifestyle prägen soll: 'Trust and never give up!'";
+	document.getElementById("band3").innerHTML = "Herbst 2014. Lorenz Imfeld stosst zur Band und ziemlich schnell fand man im Tonstudio und vor der Kamera wieder. Das Resultat ist der Videoclip von „Death is in this land“, worin die aktuelle Lage in Irak und Syrien zur Sprache kommt. Mit mittlerweile über 2500 Aufrufen auf YouTube ist Trainwood sehr stolz, so viele Leute mit dem Song erreicht und hoffentlich auch berührt zu haben.";
+	document.getElementById("band4").innerHTML = "Anfangs 2015 gibt Alfred Perner den Rücktritt aus Trainwood bekannt! Danke Alfred für deine Unterstützung und deine Energie, welche du für die Band aufgebracht hast. Ohne dich wären wir nicht dort, wo wir jetzt sind. Kurz darauf jedoch konnte mit Adrian Gander bereits einen Ersatz am Bass gefunden werden. Mit dem Ziel einer zweiten Studio-Aufnahme, dem Song 'Remember', wurde fleissig weitergeprobt.";
+	document.getElementById("band5").innerHTML = "Trainwood ist dankbar und froh, gemeinsam weitergehen zu können, trotz Schwierigkeiten und Herausforderungen. Musik ist mehr als nur das Aneinanderreihen von Rhythmen und Tönen. Sie ist ein sicherer Hafen, Ausdruck grösster Freude und Hoffnung und eine Sprache, die jeder versteht. Das macht das ganze wertvoll…";
+}
+function englishContact(){	
+	document.getElementById("contact1").innerHTML = "Order a CD, provide us some feedback about our music, order a CD or text us for booking:";
+	document.getElementById("contact2").innerHTML = "Follow us on:";
+}
+function germanContact(){
+	document.getElementById("contact1").innerHTML = "Bestelle eine CD, gib uns Rückmeldungen über unsere Musik oder schreibe uns eine Auftrittsanfrage:";
+	document.getElementById("contact2").innerHTML = "Folge uns auf:";
+}
